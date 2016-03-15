78b7b653f452aae0546b3a42beebe13777ce4459
situjun <icewating@gmail.com>
2016-03-15 14:20:56 -0400

    3rd loop correction!!
    tmp<arr[j];j-- is wrong!!!   It should be tmp<arr[j-gap];j-=gap

--- ShellTest0.java
+++ ShellTest0.java
@@ -1,11 +1,13 @@
 public class ShellTest0{
 	static public int[] ShellSort(int[] arr){
-		int gap = 0,j=0;
+		int gap = 0;
+		int j = 0;
 		//for(gap=arr.length/2;gap>0;gap=gap==2?1:(int)gap/2.2){    (int)gap/2.2 is Wrong!!! gap/2.2 is a whole block.
 		for(gap=arr.length/2;gap>0;gap=gap==2?1:(int)(gap/2.2)){
 			for(int i=gap;i<=arr.length-1;i++){
 				int tmp = arr[i];
-				for(j=i;j>=gap&&tmp<arr[j];j--){
+				// not for(j=i;j>=gap&&tmp<arr[j];j--){   tmp<arr[j];j-- is wrong!!!
+				for(j=i;j>=gap&&tmp<arr[j-gap];j-=gap){
 					arr[j]=arr[j-gap];
 				}
 				arr[j]=tmp;
